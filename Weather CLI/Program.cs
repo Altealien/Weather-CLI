e07a5fec3b436a4d4947612599077d3273cca85a
Sai <subomiomotayo@gmail.com>
2026-05-10 19:45:41 +0100

    Add error handling and logging for weather fetch logic
    Wrapped weather fetch logic in try-catch-finally to handle specific exceptions (HTTP, file not found/load) and log errors. Ensured Log.CloseAndFlush() always executes in the finally block.

--- Weather CLI/Program.cs
+++ Weather CLI/Program.cs
@@ -7,19 +7,39 @@ Log.Logger = new LoggerConfiguration()
 
 Log.Information("Weather CLI is starting...");
 
+try
+{
+    Console.Write("Enter a city: ");
+    string city = Console.ReadLine()!;
+    string secretsJson = File.ReadAllText("C:\\Users\\subom\\Desktop\\C# for Backend\\Weather CLI\\Weather CLI\\secrets.json");
+    var secrets = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(secretsJson);
+    string apiKey = secrets!.ApiKey;
 
-Console.Write("Enter a city: ");
-string city = Console.ReadLine()!;
-string secretsJson = File.ReadAllText("C:\\Users\\subom\\Desktop\\C# for Backend\\Weather CLI\\Weather CLI\\secrets.json");
-var secrets = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(secretsJson);
-string apiKey = secrets!.ApiKey;
+    string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
 
-string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+    using HttpClient client = new();
+    string response = await client.GetStringAsync(url);
 
-using HttpClient client = new();
-string response = await client.GetStringAsync(url);
-
-var weather = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(response);
-Log.Information($"City: {weather!.name} || Temperature: {weather.main.temp} || Description: {weather.weather[0].description}");
-
-Log.CloseAndFlush();
+    var weather = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(response);
+    Log.Information($"City: {weather!.name} || Temperature: {weather.main.temp} || Description: {weather.weather[0].description}");
+}
+catch (HttpRequestException e)
+{
+    Log.Error($"Failed to fetch weather data. {e.Message}");
+}
+catch (FileNotFoundException)
+{
+    Log.Error($"Failed to find secrets.json file.");
+}
+catch (FileLoadException)
+{
+    Log.Error($"Failed to load secrets.json file.");
+}
+catch (Exception e)
+{
+    Log.Error($"Unexpected error. {e.Message}");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
