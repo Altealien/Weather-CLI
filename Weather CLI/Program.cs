d9c76bfecab32081249506e665dbd498976fb7bb
Sai <subomiomotayo@gmail.com>
2026-05-10 19:38:29 +0100

    Add weather API integration and update .gitignore
    Updated .gitignore to exclude FodyWeavers.xsd and secrets.json. Enhanced Program.cs to prompt for a city, read the API key from secrets.json, fetch weather data from OpenWeatherMap, and log results using Serilog.

--- Weather CLI/Program.cs
+++ Weather CLI/Program.cs
@@ -6,4 +6,20 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 Log.Information("Weather CLI is starting...");
+
+
+Console.Write("Enter a city: ");
+string city = Console.ReadLine()!;
+string secretsJson = File.ReadAllText("C:\\Users\\subom\\Desktop\\C# for Backend\\Weather CLI\\Weather CLI\\secrets.json");
+var secrets = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(secretsJson);
+string apiKey = secrets!.ApiKey;
+
+string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+using HttpClient client = new();
+string response = await client.GetStringAsync(url);
+
+var weather = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(response);
+Log.Information($"City: {weather!.name} || Temperature: {weather.main.temp} || Description: {weather.weather[0].description}");
+
 Log.CloseAndFlush();
